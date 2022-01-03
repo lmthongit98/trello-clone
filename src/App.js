20bc694cdbd22737ce3a41d912a33415ee1b6442
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2022-01-03 16:32:17 +0700

    Design treello UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,77 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
+    <div className="trello-container">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="picsum-img" />
+              title:lmthongit98
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another cart</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="picsum-img" />
+              title:lmthongit98
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another cart</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="picsum-img" />
+              title:lmthongit98
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another cart</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="picsum-img" />
+              title:lmthongit98
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another cart</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" alt="picsum-img" />
+              title:lmthongit98
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another cart</footer>
+        </div>
+      </div>
     </div>
   );
 }
